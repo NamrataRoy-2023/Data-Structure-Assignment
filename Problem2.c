f0f8dbd96072887eb436a19f9db661eb398ca569
Namrata Roy <royrio2000@gmail.com>
2023-11-12 20:44:12 +0530

    2

--- Problem2.c
+++ Problem2.c
@@ -1,5 +1,20 @@
 ////Write a C Program to check wheather a String is Palindrome or not////
 #include <stdio.h>
+#include <string.h>
 int main(){
-	
-} 
+	char str[100];
+	int i = strlen(str) - 1;
+	int j = 0;
+	while(i>j){
+		if(str[j++] == str[i--]){
+			printf("%d is a Palindrome",str);
+			return 0;
+		}
+		else{
+			printf("%d is NOT a Palindrome",str);
+			
+		}
+		return 0;
+	}
+}
+
