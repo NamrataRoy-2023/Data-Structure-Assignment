157f851689970e1602f71a25a89b2808f6e43b34
Namrata Roy <royrio2000@gmail.com>
2023-11-14 03:14:57 +0530

    problem 11
    Write a C program to check duplicate number in an array

--- Assignment1/problem11.c
+++ Assignment1/problem11.c
@@ -1,1 +1,29 @@
 ///Write a C program to check duplicate number in an array///
+#include<stdio.h>
+int main(){
+	int n,i,j,sum = 0;
+	int arr[100];
+	int duplicate = 0;
+	printf("Enter Number of elements : ");
+	scanf("%d", &n);
+	printf("Enter array elements : \n");
+	for(i=0; i<n; i++){
+		printf("Enter Value for Index Number %d : ",i);
+		scanf("%d", &arr[i]);
+	}
+	printf("Duplicate Numbers in the array are :  ");
+	for(i=0; i<n; i++){
+		for(j=i+1; j<n; j++){
+			if(arr[i]==arr[j]){
+				printf("%d\t", arr[j]);
+				duplicate = 1;
+				break;
+			}
+			break;
+		}
+	}
+	if(!duplicate){
+		printf("There are NO Duplicate Numbers in the array");
+	}
+	return 0;
+}
