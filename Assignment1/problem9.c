00ffc71384ba87d0c2ebf468189be107d7bf48e8
Namrata Roy <royrio2000@gmail.com>
2023-11-14 02:07:34 +0530

    Write a C program to print reverse array

--- Assignment1/problem9.c
+++ Assignment1/problem9.c
@@ -1,1 +1,18 @@
 ///Write a C program to print reverse array///
+#include<stdio.h>
+int main(){
+	int n,i;
+	int arr[100];
+	printf("Enter number of elements :");
+	scanf("%d", &n);
+	printf("Enter array elements : \n");
+	for(i=0; i<n; i++){
+		printf("Enter Value for Index Number %d : ",i);
+		scanf("%d", &arr[i]);
+	}
+	printf("Reversed Array is : ");
+	for(i=arr[n-1]; i>=0; i--){
+		printf("%d\t", arr[i]);
+	}
+	return 0;
+}
