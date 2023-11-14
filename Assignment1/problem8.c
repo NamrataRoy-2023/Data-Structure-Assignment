d4d875c2c8042d3ff3c60eb6456166198fb4ea12
Namrata Roy <royrio2000@gmail.com>
2023-11-14 21:23:08 +0530

    problem 8
    Write a program that reads a 2D metrics and checks if the metrics is a symmetric metrics or no

--- Assignment1/problem8.c
+++ Assignment1/problem8.c
@@ -1,2 +1,29 @@
 ///8. Write a program that reads a 2D metrics and checks if the metrics is a symmetric metrics or not///
-
+#include <stdio.h>
+int main(){
+    int rows, cols, i, j;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("\n");
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+    // Declare array to store metric values
+    float matrix[rows][cols];
+    // Read metric from user input
+    for(i=0;i<rows;i++){
+        for(j=0;j<cols;j++){
+            printf("matrix[%d][%d]: ", i+1, j+1);
+            scanf("%f",&matrix[i][j]);
+        }
+    }
+            // Checking symmetry
+            for (i = 0; i < rows; ++i) {
+                for (j = 0; j < cols; ++j) {
+                    if (matrix[i][j] != matrix[j][i]){
+                        printf("The given matrix is not symmetric.\n");
+                        return 0;
+                    }
+                }
+            }
+            printf("The given matrix is symmetric.\n");
+}
