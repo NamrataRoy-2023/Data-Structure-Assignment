02c345dfc414cd46882368e21599abf960f934b6
Namrata Roy <royrio2000@gmail.com>
2023-11-14 21:49:44 +0530

    problem 7
    Write a program that reads two 2D metrices from the console, verifies if metrics multiplication is possible or not.Then multiplies the metrices and prints the 3rd metrics

--- Assignment1/Problem7.c
+++ Assignment1/Problem7.c
@@ -1,5 +1,60 @@
 ///Write a program that reads two 2D metrices from the console, verifies if metrics multiplication is possible or not.Then multiplies the metrices and prints the 3rd metrics///
-#include<stdio.h>
-int main(){
+#include <stdio.h>
 
+#define MAX_ROWS 10
+#define MAX_COLS 10
+
+void multiplyMatrices(int mat1[MAX_ROWS][MAX_COLS], int mat2[MAX_ROWS][MAX_COLS], int result[MAX_ROWS][MAX_COLS], int rows1, int cols1, int rows2, int cols2) {
+    for (int i = 0; i < rows1; ++i) {
+        for (int j = 0; j < cols2; ++j) {
+            result[i][j] = 0;
+            for (int k = 0; k < cols1; ++k) {
+                result[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+}
+
+void printMatrix(int mat[MAX_ROWS][MAX_COLS], int rows, int cols) {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int mat1[MAX_ROWS][MAX_COLS], mat2[MAX_ROWS][MAX_COLS], result[MAX_ROWS][MAX_COLS];
+    int rows1, cols1, rows2, cols2;
+
+    // Input for the first matrix
+    printf("Enter the number of rows and columns for the first matrix: ");
+    scanf("%d %d", &rows1, &cols1);
+
+    printf("Enter elements of the first matrix:\n");
+    for (int i = 0; i < rows1; ++i) {
+        for (int j = 0; j < cols1; ++j) {
+            scanf("%d", &mat1[i][j]);
+        }
+    }
+    // Input for the second matrix
+    printf("Enter the number of rows and columns for the second matrix: ");
+    scanf("%d %d", &rows2, &cols2);
+    if (cols1 != rows2) {
+        printf("Matrix multiplication is not possible. Number of columns in the first matrix must be equal to the number of rows in the second matrix.\n");
+        return 1;
+    }
+    printf("Enter elements of the second matrix:\n");
+    for (int i = 0; i < rows2; ++i) {
+        for (int j = 0; j < cols2; ++j) {
+            scanf("%d", &mat2[i][j]);
+        }
+    }
+    // Multiply matrices
+    multiplyMatrices(mat1, mat2, result, rows1, cols1, rows2, cols2);
+    // Print the result
+    printf("Resultant matrix after multiplication:\n");
+    printMatrix(result, rows1, cols2);
+    return 0;
 }
